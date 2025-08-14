ce06028364e26fe617bbbabfe052a39d8c906726
Het Rank <165775968+hetrank@users.noreply.github.com>
2025-08-14 11:45:59 +0530

    Add files via upload

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Header.css'
+
+function Header() {
+  return (
+    <div className='header'>
+      <div className="header-content">
+        <h2>Order your favorite food here</h2>
+        <p>Choose from a diverse menu featuring a detectable array of dishes crafted with the finest Lorem ipsum dolor sit amet, consectetur adipisicing elit. A tenetur dolorum, esse vitae quasi voluptas nemo culpa? Porro voluptatem repellendus tempora officiis ipsa obcaecati consequatur?
+        </p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
